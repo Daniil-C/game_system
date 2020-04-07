7a33401425633d53743981f57cdcfd02ca4be236
Evgeny <you@example.com>
2020-04-07 22:37:34 +0300

    Add main menu interface +full screen +exit button TODO: -settings menu -play menu

---
+++ interface/main_menu_interface.py
@@ -0,0 +1,37 @@
+""" Interface for main menu """
+import tkinter as tk
+
+root = tk.Tk()
+F = tk.Frame(borderwidth = 2, relief ="ridge", cursor = "dot", height = 1080, width = 1920, master = root)
+F.master.columnconfigure(0, weight = 1)
+F.master.rowconfigure(0, weight = 1)
+F.master.title("IMAGINARIUM")
+F.grid_propagate(0)
+F.grid(sticky = "NEWS")
+
+F.columnconfigure(0, weight = 1)
+F.columnconfigure(1, weight = 1)
+F.columnconfigure(2, weight = 1)
+F.rowconfigure(0, weight = 1)
+F.rowconfigure(1, weight = 1)
+F.rowconfigure(2, weight = 1)
+F.rowconfigure(3, weight = 1)
+F.rowconfigure(4, weight = 1)
+F.rowconfigure(5, weight = 1)
+F.rowconfigure(6, weight = 1)
+
+Game_name = tk.Label(master = F, text = "IMAGINARIUM")
+Game_name.grid(sticky = "NEWS", column = 1, row = 0)
+
+Play_button = tk.Button(master = F, text = "Play")
+# Play_button.bind("<Button-1>", fun)
+Play_button.grid(sticky = "NEWS", column = 1, row = 2)
+
+Exit_button = tk.Button(F, text = "Exit", command = root.quit)
+Exit_button.grid(sticky = "NEWS", column = 1, row = 4)
+
+Gear = tk.PhotoImage(file="settings.png")
+Settings_button = tk.Button(master = F, image = Gear)
+Settings_button.grid(sticky = "W", column = 0, row = 6)
+
+tk.mainloop()
