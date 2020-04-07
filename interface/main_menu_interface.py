2dc93e4e1bb917308843889580ef128bb605301a
Evgeny <you@example.com>
2020-04-07 22:54:32 +0300

    Fixed main menu interface

--- interface/main_menu_interface.py
+++ interface/main_menu_interface.py
@@ -1,8 +1,7 @@
 """ Interface for main menu """
 import tkinter as tk
 
-root = tk.Tk()
-F = tk.Frame(borderwidth = 2, relief ="ridge", cursor = "dot", height = 1080, width = 1920, master = root)
+F = tk.Frame(borderwidth = 2, relief ="ridge", cursor = "dot", height = 1080, width = 1920)
 F.master.columnconfigure(0, weight = 1)
 F.master.rowconfigure(0, weight = 1)
 F.master.title("IMAGINARIUM")
@@ -24,10 +23,9 @@ Game_name = tk.Label(master = F, text = "IMAGINARIUM")
 Game_name.grid(sticky = "NEWS", column = 1, row = 0)
 
 Play_button = tk.Button(master = F, text = "Play")
-# Play_button.bind("<Button-1>", fun)
 Play_button.grid(sticky = "NEWS", column = 1, row = 2)
 
-Exit_button = tk.Button(F, text = "Exit", command = root.quit)
+Exit_button = tk.Button(F, text = "Exit", command = F.quit)
 Exit_button.grid(sticky = "NEWS", column = 1, row = 4)
 
 Gear = tk.PhotoImage(file="settings.png")
