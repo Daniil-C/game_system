370ac239a0cd8fd180ab8215d43a2701dec3d9f3
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-04-09 10:36:07 +0300

    Add server listening socket initialization

--- server/__main__.py
+++ server/__main__.py
@@ -1,9 +1,17 @@
 import socket
 import os
+import work_cycle
+
 
 if __name__ == "__main__":
     LISTENING_SOCKET = socket.socket()
-    IP_ADDRESS = os.getenv["HOST_IP"]
-    PORT = os.getenv["PORT"]
+    IP_ADDRESS = os.getenv("HOST_IP", "0.0.0.0")
+    PORT = socket.htons(int(os.getenv("PORT", "7840")))
+    print("Starting game server.")
+    print("IP = ", IP_ADDRESS)
+    print("Port = ", socket.ntohs(PORT))
+    if socket.inet_aton(IP_ADDRESS) == 0 or PORT < 1024:
+        print("Wrong IP address or port")
+        exit(1)
     LISTENING_SOCKET.bind((IP_ADDRESS, PORT))
     LISTENING_SOCKET.listen()
