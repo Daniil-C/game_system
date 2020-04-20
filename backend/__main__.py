32ca43c8951f32f09c9dc4bca6b29b5305c9a8a1
Daniil <chay-chitsd@mail.ru>
2020-04-21 02:08:09 +0300

    Style fixes

--- backend/__main__.py
+++ backend/__main__.py
@@ -11,64 +11,61 @@ from monitor import Monitor
 
 
 class Player:
-	"""
-	This class provides players data
-	"""
-	def __init__(self):
-		self.cards = []
-		self.is_master = False
+    """
+    This class provides players data
+    """
+    def __init__(self):
+        self.cards = []
+        self.is_master = False
 
 
 class Common(Monitor):
-	"""
-	This class consists of common data between backend and interface
-	"""
-	def __init__(self):
-		Monitor.__init__(self)
-		self.ip = ""
-		self.port = 0
-		self.player = Player()
+    """
+    This class consists of common data between backend and interface
+    """
+    def __init__(self):
+        Monitor.__init__(self)
+        self.ip = ""
+        self.port = 0
+        self.player = Player()
 
-	def set_connection_params(self, ip, port):
-		"""
-		Set connections params to connect to game server
-		"""
-		self.ip = ip
-		self.port = port 
+    def set_connection_params(self, ip, port):
+        """
+        Set connections params to connect to game server
+        """
+        self.ip = ip
+        self.port = port
 
-	def connect(self):
-		"""
-		Connect to the server
-		"""
-		sock = socket.socket()
-		self.conn = Conn(sock)
+    def connect(self):
+        """
+        Connect to the server
+        """
+        sock = socket.socket()
+        self.conn = Conn(sock)
 
 
 class Backend(threading.Thread):
-	"""
-	This class is a backend service of game
-	"""
-	def __init__(self, common):
-		threading.Thread.__init__(self)
-		self.common = common
+    """
+    This class is a backend service of game
+    """
+    def __init__(self, common):
+        threading.Thread.__init__(self)
+        self.common = common
 
-	def start_game(self):
-		"""
-		Starts the game
-		"""
-		self.common.connect()
-
-
+    def start_game(self):
+        """
+        Starts the game
+        """
+        self.common.connect()
+        logging.info("Game started")
 
 
 
 if __name__ == "__main__":
-	com = Common()
-	com.set_connection_params("8.8.8.8", 80)
-	thread = Backend(com)
-	thread.start()
-	
-	time.sleep(1)
-	ip, port = thread.get_con_params()
-	print("ip: {}, port: {}".format(ip, port))
-	thread.join()
+    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
+    com = Common()
+    com.set_connection_params("8.8.8.8", 80)
+    thread = Backend(com)
+    thread.start()
+    thread.start_game()
+    thread.join()
