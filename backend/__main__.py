894e640843ea5b39a70c776f0fd17472b44db483
Daniil <chay-chitsd@mail.ru>
2020-04-21 01:37:04 +0300

    Add backend class

--- backend/__main__.py
+++ backend/__main__.py
@@ -3,8 +3,12 @@ This is client`s backend
 """
 
 import socket
+import logging
+import threading
+import time
 from connection import connection as Conn
 from monitor import Monitor
+
 
 class Common(Monitor):
 	"""
@@ -30,8 +34,29 @@ class Common(Monitor):
 		sock = socket.socket()
 		self.conn = Conn(sock)
 
+class Backend(threading.Thread):
+	"""
+	This class is a backend service of game
+	"""
+	def __init__(self, common):
+		threading.Thread.__init__(self)
+		self.common = common
+
+	def start_game(self):
+		"""
+		Starts the game
+		"""
+		self.common.connect()
+
+
 
 if __name__ == "__main__":
 	com = Common()
 	com.set_connection_params("8.8.8.8", 80)
-	com.connect()
+	thread = Backend(com)
+	thread.start()
+	
+	time.sleep(1)
+	ip, port = thread.get_con_params()
+	print("ip: {}, port: {}".format(ip, port))
+	thread.join()
