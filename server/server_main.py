80ff4a09f563dcc42bcb2c9fbb191385e635bf8b
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-04-21 18:34:53 +0300

    Move Monitor class into separate module

--- server/server_main.py
+++ server/server_main.py
@@ -7,27 +7,9 @@ import readline
 from select import select
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from random import shuffle, randrange
+from monitor import Monitor
 from connection import connection
 import server.environment as env
-
-
-class Monitor:
-    """
-    Base class for objects, accessable from multiple threads.
-    """
-    def __init__(self):
-        object.__setattr__(self, "_Monitor_sem", threading.Semaphore(1))
-
-    def __setattr__(self, name, value):
-        object.__getattribute__(self, "_Monitor_sem").acquire()
-        object.__setattr__(self, name, value)
-        object.__getattribute__(self, "_Monitor_sem").release()
-
-    def __getattribute__(self, name):
-        object.__getattribute__(self, "_Monitor_sem").acquire()
-        val = object.__getattribute__(self, name)
-        object.__getattribute__(self, "_Monitor_sem").release()
-        return val
 
 
 class Resources(Monitor):
